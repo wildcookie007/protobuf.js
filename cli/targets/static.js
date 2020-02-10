4455769a783deeef847df87291480c2c767dff21
Rokas Balevicius <rbalevicius@trackforce.com>
2020-02-10 15:03:36 +0200

    Change default values to null for easier code generation/consumption.

--- cli/targets/static.js
+++ cli/targets/static.js
@@ -406,16 +406,16 @@ function buildType(ref, type) {
             push(escapeName(type.name) + ".prototype" + prop + " = $util.emptyArray;"); // overwritten in constructor
         else if (field.map)
             push(escapeName(type.name) + ".prototype" + prop + " = $util.emptyObject;"); // overwritten in constructor
-        else if (field.long)
-            push(escapeName(type.name) + ".prototype" + prop + " = $util.Long ? $util.Long.fromBits("
-                    + JSON.stringify(field.typeDefault.low) + ","
-                    + JSON.stringify(field.typeDefault.high) + ","
-                    + JSON.stringify(field.typeDefault.unsigned)
-                + ") : " + field.typeDefault.toNumber(field.type.charAt(0) === "u") + ";");
+        // else if (field.long)
+        //     push(escapeName(type.name) + ".prototype" + prop + " = $util.Long ? $util.Long.fromBits("
+        //             + JSON.stringify(field.typeDefault.low) + ","
+        //             + JSON.stringify(field.typeDefault.high) + ","
+        //             + JSON.stringify(field.typeDefault.unsigned)
+        //         + ") : " + field.typeDefault.toNumber(field.type.charAt(0) === "u") + ";");
         else if (field.bytes) {
             push(escapeName(type.name) + ".prototype" + prop + " = $util.newBuffer(" + JSON.stringify(Array.prototype.slice.call(field.typeDefault)) + ");");
         } else
-            push(escapeName(type.name) + ".prototype" + prop + " = " + JSON.stringify(field.typeDefault) + ";");
+            push(escapeName(type.name) + ".prototype" + prop + " = null;");
     });
 
     // virtual oneof fields
